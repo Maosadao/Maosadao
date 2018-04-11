f3fe58ffcd65440d62e6334d23c7e336325dad1f
Maosadao <maosadao@hotmail.com>
2018-04-11 12:55:56 +0700

    Initial commit.

---
+++ src/hallo/Hallo.java
@@ -0,0 +1,13 @@
+
+package hallo;
+
+public class Hallo {
+
+    public static void main(String[] args) {
+       String []s = {"A","B","C"};
+       for(String k :s ){
+           System.out.println(k);
+       }
+    }
+    
+}
